bb04b9756690266e1a14075919a48f565b09d4aa
hhhi <fathallah.yofese@gmail.com>
2021-01-21 11:28:11 +0100

    project-youssef-fathallah

---
+++ project web sport app/projet/FindSport2Play/config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://youssef_user:BMEWkOHmmGvyjAxw@cluster0.7ibfs.mongodb.net/projet1?retryWrites=true&w=majority",
+    secretOrKey: "superSecretPass"
+};
